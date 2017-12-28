3caa3d2bf890f200ad850fb9e7b806828fdf7934
Marcel Freinbichler <marcel@freinbichler.me>
2017-12-28 18:05:40 +0100

    chore: notify on build error

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,6 +9,7 @@ import buffer from 'vinyl-buffer';
 import path from 'path';
 import del from 'del';
 import handlebars from 'gulp-compile-handlebars';
+import notifier from 'node-notifier';
 
 const $ = gulploadplugins({
   lazy: true
@@ -17,8 +18,16 @@ const $ = gulploadplugins({
 const argv = yargs.argv;
 
 function handleError(error) {
-  $.util.log(error.message);
-  $.util.log(error.codeFrame);
+  $.util.log('⚠️ ⚠️ ⚠️');
+  $.util.log($.util.colors.magenta(error.message));
+  if(error.codeFrame) {
+    $.util.log(error.codeFrame);
+  }
+  const fileName = error.filename || error.file;
+  notifier.notify({
+    title: `Error: ${fileName.split('/').pop()}`,
+    message: error.message.split(':').slice(1)
+  });
   this.emit('end');
 }
 
@@ -35,7 +44,7 @@ gulp.task('styles', () => {
      }))
     .pipe($.sass({
       precision: 10
-    }).on('error', $.sass.logError))
+    }).on('error', handleError))
     .pipe($.autoprefixer())
     .pipe(gulp.dest('.tmp'))
     // Concatenate and minify styles if production mode (via gulp styles --production)
